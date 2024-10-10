bb201311b26895a46861a26620a7d8ede8be978b
Gamezad_ <abolfazlshaban8787@outlook.com>
2024-10-10 05:00:33 +0330

    base files

---
+++ scripts/Things.js
@@ -0,0 +1,79 @@
+import { gameStarted, speed as mainSpeed } from "./app.js";
+const character = document.querySelector("#object");
+const blockFrame = document.querySelector("#blockFrame");
+
+export class RunnerObject {
+  name;
+
+  src= '';
+  speed ;
+  stopRunV = false;
+  constructor(name,myspeed) {
+    this.name = name;
+    this.speed = myspeed;
+    
+  }
+  run(){
+    this.stopRunV = false
+    let timer = setInterval(() => { 
+        if(this.stopRunV) clearInterval(timer)
+        if(!gameStarted) clearInterval(timer)
+        for (let i = 0; i < blockFrame.children.length; i++) {
+          let item = blockFrame.children[i];
+          if (item.getAttribute("data") != this.name) continue;
+    
+          const rightCalc = parseInt(item.style.right) + mainSpeed;
+          item.style.right = (rightCalc + this.speed) + "px";
+    
+          const charLeft = character.offsetLeft;
+          const charW = character.offsetWidth;
+          const blockLeft = item.offsetLeft;
+          const blockW = item.offsetWidth;
+    
+          const charTop = character.offsetTop;
+          const charH = character.offsetHeight;
+          const blockTop = item.offsetTop;
+          const blockH = item.offsetHeight;
+
+          if (item?.offsetLeft < 0) {
+            item.remove();
+          }
+    
+          if (
+            charLeft + charW > blockLeft && !(blockLeft + blockW < charLeft) && charTop + charH > blockTop && !(blockTop + blockH < charTop)
+          ) {
+            if(!item.getAttribute('called')){
+                item.setAttribute('called',true)
+                if(this.action) this.action(item);
+            }
+          }
+        }
+      }, 25 );
+
+      
+  }
+
+
+    stopRun() {
+    this.stopRunV = true
+     }
+
+  action;
+
+  createData = {width: 30,height:30,classes: '',src: '', style: ''} 
+  create() {
+    blockFrame.innerHTML += `<${this.createData.src == '' ? 'div' : `img src=${this.createData.src}`} style="right: 0; ${this.createData.style} " data="${this.name}" class=" ${this.createData.classes} w-[${this.createData.width}px] h-[${this.createData.height}px] absolute"></${this.createData.src == '' ? 'div' : 'img'}>`;
+  }
+  removeLast(called = false){
+    for (let i = 0; i < blockFrame.children.length; i++) {
+        if(blockFrame.children[i].getAttribute('data') == this.name){
+            if(called) if (!blockFrame.children[i].hasAttribute('called')) continue;
+            blockFrame.children[i]?.remove()
+            break;
+        }
+        
+    }
+    
+    
+  }
+}
---
+++ scripts/app.js
@@ -0,0 +1,253 @@
+import { RunnerObject } from "./Things.js";
+
+const jumpBtn = document.getElementById("jumpBtn");
+const downBtn = document.getElementById("downBtn");
+
+
+const backdrop = document.querySelector("#backdrop");
+const dead = document.querySelector("#dead");
+const character = document.querySelector("#object");
+const startBtn = document.getElementById("startBtn");
+const restartBtn = document.getElementById("restartBtn");
+
+const frame = document.querySelector(".moveFrame");
+const blockFrame = document.querySelector("#blockFrame");
+const scoreText = document.querySelector("#score");
+const coinsText = document.querySelector("#coins");
+const ScoreTitle = document.querySelector("#ScoreTitle");
+const bestScoreText = document.querySelector("#Bscore");
+
+document.querySelectorAll("#bestScoreTitle")[0].innerHTML =
+  localStorage.getItem("bestScore") ?? 0;
+document.querySelectorAll("#bestScoreTitle")[1].innerHTML =
+  localStorage.getItem("bestScore") ?? 0;
+
+let blocks = [];
+let clouds = [];
+
+export let gameStarted = false;
+
+let jump = false;
+let down = false;
+let jumpC = false;
+
+let jumpValue = 0.1;
+export let speed = 2;
+
+let Score = 0;
+let Coin = 0;
+let BestScore = localStorage.getItem("bestScore") ?? 0;
+coinsText.innerHTML = localStorage.getItem("coins") ?? 0;
+Coin = parseInt(localStorage.getItem("coins") ?? 0);
+
+const BlockRunner = new RunnerObject("block", 3);
+const CloudRunner = new RunnerObject("cloud", 1);
+const CoinRunner = new RunnerObject("coin", 3);
+
+startBtn.addEventListener("click", () => {
+  startGame();
+});
+restartBtn.addEventListener("click", () => {
+  restartGame();
+});
+
+let DefaultnumY = 4;
+bestScoreText.innerHTML = localStorage.getItem("bestScore") ?? 0;
+
+function startGame() {
+  StartHandler();
+
+  backdrop.classList.add("hidden");
+
+  let numY = DefaultnumY;
+
+  character.attributes.removeNamedItem("hidden");
+  let tempY = DefaultnumY;
+  setInterval(() => {
+
+    if (!gameStarted) return;
+
+    if (tempY < 0) jump = false;
+
+    if (jump) {
+      if (numY < frame.offsetHeight - character.offsetHeight - 10) {
+        tempY -= jumpValue;
+        numY += Math.max(tempY, 0);
+      }
+    } else {
+      if (numY > 0) {
+        tempY += jumpValue;
+        numY -= tempY;
+      } else {
+        tempY = DefaultnumY;
+        numY = 0;
+        jumpC = false;
+      }
+    }
+
+    if (down) {
+      if (numY > DefaultnumY) {
+        numY -= 8;
+      } else {
+        jumpC = false;
+        jump = false;
+        down = false;
+      }
+    }
+
+    character.style.left = `80px`;
+    character.style.bottom = `${numY}px`;
+  }, 13);
+}
+
+function restartGame() {
+  Score = 0;
+  blocks = [];
+  blockFrame.innerHTML = "";
+  speed = 2;
+  dead.classList.add("hidden");
+
+  character.classList.add("animate-spin");
+
+  gameStarted = true;
+  character.style.bottom = 0;
+  StartHandler();
+}
+
+function scoreHandler() {
+  setInterval(() => {
+    if (!gameStarted) return;
+    Score += speed;
+    ScoreTitle.innerHTML = Math.round(Score);
+    scoreText.innerHTML = Math.round(Score);
+
+    document.querySelectorAll("#bestScoreTitle")[0].innerHTML = BestScore;
+    document.querySelectorAll("#bestScoreTitle")[1].innerHTML = BestScore;
+
+    if (Score > BestScore) {
+      BestScore = Math.round(Score);
+      bestScoreText.innerHTML = Math.round(Score);
+    }
+  }, 100);
+
+  setInterval(() => {
+    if (gameStarted) speed += 0.05;
+  }, 2000);
+}
+
+function StartHandler() {
+  gameStarted = true;
+  RunBlocks();
+  RunClouds();
+  RunCoins();
+  scoreHandler();
+}
+function StopHandler() {
+  gameStarted = false;
+  jump = false;
+  jumpC = false;
+  localStorage.setItem("bestScore", Math.round(BestScore));
+  localStorage.setItem("coins", Coin);
+  dead.classList.remove("hidden");
+
+  character.classList.remove("animate-spin");
+}
+
+function RunBlocks() {
+  BlockRunner.run();
+
+  BlockRunner.action = () => {
+    StopHandler();
+  };
+
+  let num = 0;
+  loop();
+
+  function loop() {
+    setTimeout(() => {
+      if (gameStarted) {
+        BlockRunner.createData.height = Math.random() * 50 + 10;
+        BlockRunner.createData.classes = `bg-red-500 bottom-0`;
+        BlockRunner.create();
+
+        num = Math.random() * 5000;
+        loop();
+      }
+    }, (num - speed * 500 )+ 1000);
+  }
+}
+
+function RunClouds() {
+  CloudRunner.run();
+  let num = 0;
+  loop();
+
+  function loop() {
+    setTimeout(() => {
+      if (gameStarted) {
+        CloudRunner.createData.src = "cloud.png";
+        CloudRunner.createData.classes = `top-[${
+          Math.random() * 50 + 10
+        }px] scale-[${Math.random() * 2 + 1}] opacity-[${Math.random()}]`;
+        CloudRunner.create();
+
+        num = Math.random() * 4000;
+        loop();
+      }
+    }, num + 500);
+  }
+}
+
+function RunCoins() {
+  CoinRunner.run();
+  let num = 0;
+  loop();
+
+  function loop() {
+    setTimeout(() => {
+      if (gameStarted) {
+        CoinRunner.createData.src = "coin.png";
+        CoinRunner.createData.classes = `scale-[2] transition-all`;
+        CoinRunner.createData.style = `bottom: ${
+          Math.random() > 0.3 ? "5" : "50"
+        }px;transition-property: transform,opacity; transition-duration: 400ms;`;
+        CoinRunner.create();
+
+        CoinRunner.action = (item) => {
+          item.classList.add("-translate-y-36", "opacity-0");
+          Coin++;
+
+          coinsText.innerHTML = Coin;
+        };
+
+        num = Math.random() * 5000;
+        loop();
+      }
+    }, num + 1000);
+  }
+}
+
+window.addEventListener("keydown", (e) => {
+  if (e.key == "ArrowUp" && !jumpC) {
+    jump = true;
+    jumpC = true;
+  }
+});
+window.addEventListener("keydown", (e) => {
+  if (e.key == "ArrowDown") {
+    down = true;
+  }
+});
+
+jumpBtn.addEventListener("click", () => {
+  if(!jumpC){
+    jump = true;
+    jumpC = true;
+  }
+    
+});
+
+downBtn.addEventListener("click", () => {
+    down = true;
+  
+});
